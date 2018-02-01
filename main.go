b3890170b6880250a2d923e54409bcda69595207
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-02-02 00:13:42 +0600

    Func Create

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -47,7 +49,12 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 
 // Crate a New Book
 func createBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(100000)) // Mock ID - not safe
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
 }
 
 //
